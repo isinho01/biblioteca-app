915ff837303354df1fcb96692e079c660e255329
Isaac Oliveira <135730107+isinho01@users.noreply.github.com>
2025-06-23 11:34:58 -0300

    update do cabeçalho

--- src/routes/cabecalho/cabecalhoteste.svelte
+++ src/routes/cabecalho/cabecalhoteste.svelte
@@ -1,6 +1,19 @@
-<h1>Sistema gerenciador de livros</h1>
 
-<li>Teste </li>
-<li>Teste</li>
-<li>Teste</li>
-<li>Testeeeeeee</li>
+ <header class="header">
+    <h1>Sistema gerenciador de livros</h1>
+        <nav class="nav">
+            <ul class="nav-list">
+                <li><a href="#">Livros disponiveis</a></li>
+                <li><a href="#">Renovar emprestimo</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <br>
+
+<!-- Estilização do componente (Evelyn) -->
+<style>
+    header{
+        background-color: gray;
+    }
+</style>

de8c286f2bc4e975ef0ade003d323d52a642db0b
Isaac Oliveira de melo <isaac.o@escolar.ifrn.edu.br>
2025-07-14 09:57:40 -0300

    update do script

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
+<!-- importando o componente do cabeçalho pra pagina principal -->
+<script>
+    import Cabecalhoteste from "./cabecalho/cabecalhoteste.svelte";
+</script>
+
+
 <!-- utilizando o componente duas vezes como teste -->
 <Cabecalhoteste/>
 
 <img src="https://user-images.githubusercontent.com/6876788/96633009-d1818000-1318-11eb-9f1d-7f914f4ccb16.gif" alt="">
 
 
-<!-- importando o componente do cabeçalho pra pagina principal -->
-<script>
-    import Cabecalhoteste from "./cabecalho/cabecalhoteste.svelte";
-</script>
 
 

24bd6f4ae4d1f5a82534dd0c2bb05e1d9534805b
Joao Pedro <rocha.araujo@escolar.ifrn.edu.br>
2025-07-14 10:29:14 -0300

    uptade menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <!-- importando o componente do cabeçalho pra pagina principal -->
 <script>
     import Cabecalhoteste from "./cabecalho/cabecalhoteste.svelte";
+    import Menuteste from "./menu/menu.svelte";
+
 </script>
+
+
 
 
 <!-- utilizando o componente duas vezes como teste -->
 <Cabecalhoteste/>
 
-<img src="https://user-images.githubusercontent.com/6876788/96633009-d1818000-1318-11eb-9f1d-7f914f4ccb16.gif" alt="">
-
-
+<Menuteste/>
 
 

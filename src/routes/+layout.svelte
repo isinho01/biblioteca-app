fbb6fb6d952c3f6bf84c9e4bad3d6cc5f49943c1
Isaac Oliveira de melo <isaac.o@escolar.ifrn.edu.br>
2025-07-18 10:30:34 -0300

    update componentes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import '../app.css';
-
+	import Cabecalhoteste from "./cabecalho/cabecalhoteste.svelte";
 	let { children } = $props();
 </script>
 
+<Cabecalhoteste/>
+<!-- Usado pra carregar componente antes de outros componentes -->
 {@render children()}
-
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,7 @@
-<!-- importando o componente do cabeçalho pra pagina principal -->
 <script>
-    import Cabecalhoteste from "./cabecalho/cabecalhoteste.svelte";
     import Menuteste from "./menu/menu.svelte";
     import Rodapeteste from "./rodape/rodape.svelte";
 </script>
 
-
-
-
-<!-- utilizando o componente duas vezes como teste -->
-<Cabecalhoteste/>
-
 <Menuteste/>
-
 <Rodapeteste/>

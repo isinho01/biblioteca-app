9b34683786c8adeb7c215a9af7f88c854538358a
Evelyn <evelyn.andrade@escolar.ifrn.edu.br>
2025-07-17 22:18:03 -0300

    minhas alterações

--- src/routes/cabecalho/cabecalhoteste.svelte
+++ src/routes/cabecalho/cabecalhoteste.svelte
@@ -1,23 +1,38 @@
 <div class="principal">
 	<img src="/Campus São Gonçalo do Amarante - Logo_Color Hor.png" alt="Logo do campus" />
 	<h1>Sistema Bibliotecario | IFRN Campus São Gonçalo do Amarante</h1>
-	<ul class="listas">
+</div>
+<div class ="bloco">
+		<ul class="listas">
+			<li>Buscar Livro</li>
 		<li>Livros disponiveis para emprestimo</li>
 		<li>Renovar emprestimo</li>
-		<li></li>
+
 	</ul>
 </div>
 
 <!-- Estilização do componente (Evelyn) -->
 <style>
 	h1 {
-		font-size: 40px;
+		font-size: 25px;
+		text-align: center;
 	}
 	.listas {
-		padding: 4px;
-		gap: 4px;
+		margin-left: 20%;
+		text-align: center;
+		display: flex;
+		list-style: none;
+		gap: 40px;	
+	}
+	.listas  {
+		margin:0;
+		display:flex;
+		justify-content: center;
+		padding: 5px;
+		background-color: #f0f0f0; 
 	}
 	.principal {
+		padding: 3px;
 		background-color: #228b22;
 	}
 </style>

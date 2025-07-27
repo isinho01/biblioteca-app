284d91ddb4b72ee0be0e7f8ecbeb03b07564364d
Joyce <148766405+j3rodrigues@users.noreply.github.com>
2025-07-27 21:04:09 +0000

    destaque

--- src/routes/destaques/+page.svelte
+++ src/routes/destaques/+page.svelte
@@ -1,6 +1,90 @@
-<script>
-	import Menu from "$lib/componentes/Menu.svelte";
+<div class="box">
+	<div class="img">
+		<img src="/domcasmurrolivro.jpg" alt="">
+		<p>Descrição do Livro</p>
+		<p>Escritor(a)</p>
+		<p>Ano de publicação</p>
+	</div>
+	<div class="img">
+		<img src="/aguavivalivro" alt="">
+		<p>Descrição do Livro</p>
+		<p>Escritor(a)</p>
+		<p>Ano de publicação</p>
+	</div>
+	<div class="img">
+		<img src="/Lacos-de-Familia.png" alt="">
+		<p>Descrição do Livro</p>
+		<p>Escritor(a)</p>
+		<p>Ano de publicação</p>
+	</div>
+</div>
 
-</script>
-<Menu titulo="Destaques IMP"
-subtitulo="confira os destaques"/>
+<div class="box">
+	<div class="img">
+		<img src="/linuxlivro.png" alt="">
+		<p>Descrição do Livro</p>
+		<p>Escritor(a)</p>
+		<p>Ano de publicação</p>
+	</div>
+	<div class="img">
+		<img src="/pequenacoreografialivro.png" alt="">
+		<p>Descrição do Livro</p>
+		<p>Escritor(a)</p>
+		<p>Ano de publicação</p>
+	</div>
+	<div class="img"><img src="/Lacos-de-Familia.png" alt="">
+		<p>Descrição do Livro</p>
+		<p>Escritor(a)</p>
+		<p>Ano de publicação</p>
+	</div>
+</div>
+
+<div class="box">
+	<div class="img">
+		<img src="/Lacos-de-Familia.png" alt="">
+		<p>Descrição do Livro</p>
+		<p>Escritor(a)</p>
+		<p>Ano de publicação</p>
+	</div>
+	<div class="img">
+		<img src="/Lacos-de-Familia.png" alt="">
+		<p>Descrição do Livro</p>
+		<p>Escritor(a)</p>
+		<p>Ano de publicação</p>
+	</div>
+	<div class="img">
+		<img src="/Lacos-de-Familia.png" alt="">
+		<p>Descrição do Livro</p>
+		<p>Escritor(a)</p>
+		<p>Ano de publicação</p>
+	</div>
+</div>
+
+<style>
+	.box{
+		display: flex;
+		justify-content: center;
+		flex-wrap: wrap;
+		margin: 4px;
+		gap: 20px;
+		margin-bottom: 40px;
+	}
+	.img {
+		width: 300px;
+		border-radius: 20px; /* Arredonda as pontas */
+		background-color: rgba(208, 203, 203, 0.718);
+		padding: 10px;
+		text-align: center;
+	}
+
+	.img img {
+		width: 300px;
+		height: 400px;
+	}
+
+	.img p {
+		margin-top: 10px;
+		font-size: 16px;
+		color: black;
+	}
+</style>

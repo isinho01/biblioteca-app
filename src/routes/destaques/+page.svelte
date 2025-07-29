9e92f2ab1b812dc4ce045a657cbd4bf60b9d01f9
Isaac Oliveira <135730107+isinho01@users.noreply.github.com>
2025-07-29 20:20:57 -0300

    Adicionei 3 livros na div 2

--- src/routes/destaques/+page.svelte
+++ src/routes/destaques/+page.svelte
@@ -21,21 +21,22 @@
 
 <div class="box">
 	<div class="img">
-		<img src="/linuxlivro.png" alt="">
-		<p>Descrição do Livro</p>
-		<p>Escritor(a)</p>
-		<p>Ano de publicação</p>
+		<img src="/Livrosdestaque/umadelicadaalinebei.jpg" alt="">
+		<p>Numa humilde casa de portão laranja em Belva, vivem avó e neta. Enquanto Laura vai à escola, brinca com as amigas e pouco a pouco se despede da infância, Margarida lê mãos numa feira de antiguidades para prover o sustento da família. A avó se devota por completo ao cuidado da menina, elo mais forte com sua filha, com quem não tem contato. Da mesma forma, Laura entrega a Margarida o amor desmedido de uma criança.
+O universo particular das duas, porém, se quebra com a chegada de Filipa, bisavó de Laura e mãe de Margarida. A idosa, que perdera o lugar onde morava, logo se apodera dos pequenos cômodos com suas caixas e amarguras.</p>
+		<p>Aline Bei</p>
+		<p>2025</p>
 	</div>
 	<div class="img">
-		<img src="/pequenacoreografialivro.png" alt="">
-		<p>Descrição do Livro</p>
-		<p>Escritor(a)</p>
-		<p>Ano de publicação</p>
+		<img src="/Livrosdestaque/anasuyagente.jpg" alt="">
+		<p>Escrito a partir de diálogos, A gente mira no amor e acerta na solidão, surgiu de experiências vividas pela autora em salas de aula, em sessões de análise (enquanto analisante ou analista), com amigos, em leituras de pesquisas teóricas. Neste livro, a psicanalista e professora Ana Suy quer, acima de tudo, continuar essa conversa contigo, leitor, sem a pretensão, no entanto, de ser um manual ou um tratado acadêmico sobre o tema.</p>
+		<p>Ana Suy</p>
+		<p>2022</p>
 	</div>
-	<div class="img"><img src="/Lacos-de-Familia.png" alt="">
-		<p>Descrição do Livro</p>
-		<p>Escritor(a)</p>
-		<p>Ano de publicação</p>
+	<div class="img"><img src="/Livrosdestaque/anaturezacarla.jpeg" alt="">
+		<p>“O que você não tem mais que te entristece tanto?” É com esta pergunta que Biá, uma psicanalista aposentada, apaixonada por literatura, aborda a jovem jornalista Olívia pela primeira vez ao encontrá-la sentada à mesa de um sebo improvisado. A provocação inesperada, vinda de uma estranha, capaz de ouvir “como quem abraça", desencadeia uma sucessão de encontros, marcados pela intimidade crescente e que aos poucos revelam as histórias das duas mulheres. “Nossa amizade começou assim, enquanto nos afogávamos”, relata Olívia.</p>
+		<p>Carla Madeira</p>
+		<p>2022</p>
 	</div>
 </div>
 
